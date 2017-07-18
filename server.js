5932ac18255d90d3d5600bc3a9a930f6ef9e4128
Oksana Harris <oksanaharris@Oksanas-MacBook-Pro.local>
2017-07-17 20:04:29 -1000

    working on PUT - almost done but something is wrong

--- server.js
+++ server.js
@@ -2,10 +2,6 @@ const http = require('http');
 const fs = require('fs');
 const querystring = require('querystring');
 
-// console.log(http);
-
-// console.log('DIRECTORY');
-
 var elementListArr = [];
 var validFileNames = ['/hydrogen.html', '/helium.html', '/', '/index.html', , ' ', '/css/styles.css'];
 
@@ -13,8 +9,6 @@ fs.readdir('./public/', (err, files) => {
   if (err){
     console.log('Directory look up error');
   } else {
-    // console.log('DIRECTORY FILES');
-    // console.log(files);
 
     var filesToIgnore = ['404.html', '.keep', '.DS_Store', 'css', 'index.html'];
 
@@ -33,18 +27,14 @@ fs.readdir('./public/', (err, files) => {
 
     const server = http.createServer(requestHandler);
     server.listen(8080);
-
-    // console.log('INSIDE THE FS READ DIR');
-    // console.log(elementListArr);
   }
 });
 
 function requestHandler (request, response){
-  console.log('SET UP OUR SERVER');
+  console.log('Got a request - ', request.method);
 
-  console.log(elementListArr);
+  console.log('starting list of element files', elementListArr);
 
-  //checking if url stipulates a valid file name
   if (request.method === 'GET'){
 
     if (validFileNames.indexOf(request.url) != -1){
@@ -63,28 +53,10 @@ function requestHandler (request, response){
     response.writeHead(200);
   }
 
-
-
-  //if it's a GET request, we want to serve the contents of files
-  //this means we gots to reads the contents of files first - prolly using fs.readFile, I'm guessing?!?!?
-  //
-
-
-
-  // response.write('<html>');
-  // response.write('<body>');
-  // response.write('<h1>Hi bitchezzz!</h1>');
-  // response.write('</body>');
-  // response.write('</html>');
-
-  // console.log(request.method, request.url);
-
   var body = [];
 
   request.on('data', (chunk) => {
     body.push(chunk);
-    // console.log('this is the console log from DATA');
-    // console.log(body);
   });
 
   request.on('end', () => {
@@ -92,11 +64,27 @@ function requestHandler (request, response){
     console.log('this is the console log from END');
     console.log(body);
 
-    if (request.method === 'POST' && request.url === '/elements') {
+    if ((request.method === 'POST' && request.url === '/elements') || (request.method === 'PUT' && validFileNames.indexOf(request.url) !== -1)){
+
       var parsedObj = querystring.parse(body);
-      console.log('PARSED');
-      console.log(parsedObj);
-      createNewElementFile(request, response, parsedObj);
+
+      var requiredFields = ['elementName', 'elementSymbol', 'elementAtomicNumber', 'elementDescription'];
+
+      if (requiredFields.every(field => field in parsedObj && parsedObj[field] !== '')) {
+        createNewElementFile(request, response, parsedObj);
+      } else {
+        console.log('error - not providing all of the required fields');
+        response.writeHead(500, {'content-Type': 'application/JSON'});
+        response.write(JSON.stringify({'error': 'not all of the required fields were provided'}));
+        response.end();
+      }
+    }
+
+    if (request.method === 'PUT' && validFileNames.indexOf(request.url) === -1){
+      response.writeHead(500, {'content-Type': 'application/JSON'});
+      response.write(JSON.stringify({'error': 'resource does not exist'}));
+      console.log('error - trying to modify a non-existing file');
+      response.end();
     }
 
   });
@@ -113,51 +101,18 @@ function requestHandler (request, response){
 }
 
 
-
-// server.on('connect', (req, cltSocket, head) => {
-//   console.log('picking up a connection');
-
-//   clientConnection.on('data', (input) =>{
-//     console.log(input.toString());
-//   })
-// });
-
-
-  // var status = 200;
-
-  // var validPathArr = ['/index.html', '/hydrogen.html', '/helium.html', '/css/styles.css'];
-  // if (validPathArr.indexOf(path) === -1){
-  //   if (path === '' || path === ' ' || path === '/'){
-  //     path = '/index.html';
-  //   } else {
-  //     path = '/404.html';
-  //     status = 404;
-  //   }
-  // }
-
-
 function streamFileContents(url, response){
 
   fs.readFile(('./public/' + url), function read(err, data) {
       if (err) {
         console.log('errrrrrroooorrrr');
-        // connection.write('HTTP/1.1 500 ERROR');
+        response.write('HTTP/1.1 500 ERROR');
       } else {
-        // console.log('DATA FROM streamFileContent');
-        // console.log(data.toString());
         response.write(data.toString());
         response.end();
       }
-    // connection.end();
   });
 }
-
-// server.on('request', (request, response) => {
-//   console.log('got a request');
-
-//   console.log(request.url);
-//   response.end();
-// });
 
 function createNewElementFile (request, response, elementObj){
   var elementFilePath = './public/' + elementObj.elementName.toLowerCase() + '.html';
@@ -178,27 +133,29 @@ function createNewElementFile (request, response, elementObj){
 </body>
 </html>`;
 
-elementListArr.push(elementObj.elementName.toLowerCase());
-console.log('now the element array looks like this');
-console.log(elementListArr);
+  if (request.method === 'POST'){
+    validFileNames.push('/'+elementObj.elementName.toLowerCase()+'.html');
+    elementListArr.push(elementObj.elementName.toLowerCase());
+    console.log('now the element array looks like this');
+    console.log(elementListArr);
+  }
 
-validFileNames.push('/'+elementObj.elementName.toLowerCase()+'.html');
-
-var elementsListStr = '';
-elementListArr.forEach((element, index, array,) => {
-  var elementTitle = element.charAt(0).toUpperCase() + element.substring(1);
-  elementsListStr += `
-    <li>
-      <a href="/${element}.html">${elementTitle}</a>
-    </li>`
-  ;
-});
+  var elementsListStr = '';
+  elementListArr.forEach((element, index, array,) => {
+    var elementTitle = element.charAt(0).toUpperCase() + element.substring(1);
+    elementsListStr += `
+      <li>
+        <a href="/${element}.html">${elementTitle}</a>
+      </li>`
+    ;
+  });
 
   fs.writeFile(elementFilePath, elementStringTemplate, (err) => {
     if (err) {
       response.writeHead(500, {'content-Type': 'application/JSON'});
       response.write(JSON.stringify({'success': false}));
       console.log(err);
+      response.end();
     } else {
       console.log('The file has been saved.');
       modifyIndexFile (request, response, elementsListStr);
@@ -229,6 +186,7 @@ function modifyIndexFile (request, response, elementListStr){
       response.writeHead(500, {'content-Type': 'application/JSON'});
       response.write(JSON.stringify({'success': false}));
       console.log(err);
+      response.end();
     } else {
       response.writeHead(200, {'content-Type': 'application/JSON'});
       response.write(JSON.stringify({'success': true}));
